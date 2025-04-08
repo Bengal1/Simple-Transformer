b36f01d174384f5ca516297e5099bc9ad97a5761
Bengal1 <orbengal1@gmail.com>
2025-04-08 17:54:28 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	train.py

--- models/SimpeTransformer.py
+++ models/SimpeTransformer.py
@@ -427,7 +427,7 @@ class SimpleTransformer(torch.nn.Module):
                 # Predict next token
                 out_logits = self.w_o(dec_out[:, -1, :])
                 probs = self.softmax(out_logits)
-                next_token = out_logits.argmax(dim=-1, keepdim=True)
+                next_token = probs.argmax(dim=-1, keepdim=True)
 
                 # Append predicted token to sequence
                 trg_seq = torch.cat([trg_seq, next_token], dim=1)
