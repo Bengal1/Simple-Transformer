d38be17350747d221f90f9a0d8b4aeb7ab727228
Bengal1 <Orbengal1@gmail.com>
2025-07-14 02:19:39 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	train.py

--- models/SimpleTransformer.py
+++ models/SimpleTransformer.py
@@ -501,9 +501,10 @@ class Decoder(nn.Module):
 
         return dec_out
 
-from layers.PositionalEncoding import PositionalEncoding
-from layers.Encoder import Encoder
-from layers.Decoder import Decoder
+
+# from layers.PositionalEncoding import PositionalEncoding
+# from layers.Encoder import Encoder
+# from layers.Decoder import Decoder
 
 
 class SimpleTransformer(nn.Module):
